6fe4db34f8de3e098bf1ecde630a969d6f046f3e
Amid <azorty71@gmail.com>
2022-07-04 00:45:19 +0300

    add filter

---
+++ src/components/ShowFullitem.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class ShowFullitem extends Component {
+    render() {
+            return (
+            <div>ShowFullitem</div>
+            )
+    }
+}
+
+export default ShowFullitem
