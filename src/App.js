d154d890b34116418563c1d21013beb0a5b12e00
Amid <azorty71@gmail.com>
2022-07-03 22:00:25 +0300

    add delete button and functions

--- src/App.js
+++ src/App.js
@@ -60,21 +60,27 @@ class App extends React.Component {
       ]
     }
     this.addToOrder = this.addToOrder.bind(this)
+    this.deleteOrder = this.deleteOrder.bind(this)
   }
   render() {
     return (
       <div className="wrapper">
-        <Header orders={this.state.orders}/>
+        <Header orders={this.state.orders} onDelete={this.deleteOrder}/>
         <Items items={this.state.items} onAdd={this.addToOrder}/>
         <Footer />
       </div>
     );
   }
 
+  deleteOrder(id) {
+    // console.log(id);
+      this.setState({ orders: this.state.orders.filter(el => el.id !== id) })
+  }
+
   addToOrder(item) {
     let isInArray = false
     this.state.orders.forEach(el => {
-      if (el.id == item.id)
+      if (el.id === item.id)
         isInArray = true
     })
     if (!isInArray)
