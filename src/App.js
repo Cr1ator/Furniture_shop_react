bdd64b17743cf76fd2bdee2748faeddf34590129
Amid <azorty71@gmail.com>
2022-07-03 17:42:04 +0300

    add tovar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,73 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Items from "./components/Items";
+import React from "react";
 
-function App() {
-  return (
-    <div className="wrapper">
-      <Header />
-      <Footer />
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1, 
+          title: 'Стул серый', 
+          img: 'chair-grey, jpeg', 
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2, 
+          title: 'Стол', 
+          img: 'table.webp', 
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'tables', 
+          price: '149.00'
+        },
+        {
+          id: 3, 
+          title: 'Диван', 
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'sofa', 
+          price: '549.99'
+        },
+        {
+          id: 4, 
+          title: 'Стул серый', 
+          img: 'chair-grey, jpeg', 
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2, 
+          title: 'Стол', 
+          img: 'table.webp', 
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'tables', 
+          price: '149.00'
+        },
+        {
+          id: 3, 
+          title: 'Диван', 
+          img: 'sofa.jpeg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.', 
+          category: 'sofa', 
+          price: '549.99'
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div className="wrapper">
+        <Header />
+        <Items items={this.state.items}/>
+        <Footer />
+      </div>
+    );
+  }
 }
 
 export default App;
